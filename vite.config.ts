09302a533e93376ff12b1e254b7de1bbe5433125
mexil <m1229513@gmail.com>
2024-07-24 10:00:12 +0200

    solve types problem on import library and clean unused files

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,11 @@ import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ exclude: ['**/*.stories.tsx'] })],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ exclude: ['**/*.stories.tsx'] })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
